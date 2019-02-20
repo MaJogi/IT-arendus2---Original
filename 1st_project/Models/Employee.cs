38acad041359ee352c557194ecfad6f0f906a2c3
MaJogi <47116426+MaJogi@users.noreply.github.com>
2019-02-20 14:29:39 +0200

    Starting out

---
+++ 1st_project/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace _1st_project1st_project.Models
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Salary { get; set; }
+    }
+}
---
+++ Labor/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Labor.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Labor.Controllers
+{
+    public class TestController : Controller
+    {
+        public string GetString()
+        {
+            return "Hello world!";
+        }
+
+        public ActionResult GetView()
+        {
+            Employee emp = new Employee();
+            emp.FirstName = "Sukesh";
+            emp.LastName = "Marla";
+            emp.Salary = 20000;
+
+            ViewData["Employee"] = emp;
+            return View("MyView");
+        }
+    }
+}
