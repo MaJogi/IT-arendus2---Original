7f9045218089f88b65dc2aecfdb8bb5c23c74ee8
MaJogi <47116426+MaJogi@users.noreply.github.com>
2019-02-22 00:10:28 +0200

    am

--- Infra/Employees.cs
+++ Infra/Employees.cs
@@ -1,23 +1,16 @@
 ﻿using Core;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Infra
 {
     public static class Employees
     {
-        public static List<Employee> Get()
+        public static List<Employee> Get(SalesDbContext db)
         {
-            var employees = new List<Employee>
-            {
-                new Employee("John", "Doe", 14000),
-                new Employee("Michael", "Jackson", 16000),
-                new Employee("Robert", "Pattinson", 20000)
-            };
-            return employees;
-
-
+            return db.Employees.ToList();
         }
     }
 }
