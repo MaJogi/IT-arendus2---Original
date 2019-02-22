0c5192eedce1af8fac8e3ca5a3a9116a1045746c
MaJogi <47116426+MaJogi@users.noreply.github.com>
2019-02-22 13:12:32 +0200

    Renamed index employee

--- Week4/Labor/Controllers/EmployeeController.cs
+++ Week4/Labor/Controllers/EmployeeController.cs
@@ -6,12 +6,12 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Labor.Controllers
 {
-    public class TestController : Controller
+    public class EmployeeController : Controller
     {
         private readonly SalesDbContext db;
 
-        public TestController(SalesDbContext db) {this.db = db; }
-        public ActionResult GetView()
+        public EmployeeController(SalesDbContext db) {this.db = db; }
+        public ActionResult Index()
         {
             var model = new EmployeeListViewModel();
             var employees = Employees.Get(db);
@@ -23,9 +23,14 @@ namespace Labor.Controllers
             }
 
             model.Employees = list;
-            model.UserName = "Admin";
+            
            
-            return View("MyView", model);
+            return View("Index", model);
+        }
+
+        public ActionResult AddNew()
+        {
+            return View("CreateEmployee");
         }
     }
 }
