493a861c5520dad8f4cd19d4f645e1b8866927d8
MaJogi <47116426+MaJogi@users.noreply.github.com>
2019-02-23 11:05:08 +0200

    Added Labor12

--- Infra/Employees.cs
+++ Infra/Employees.cs
@@ -6,11 +6,18 @@ using System.Text;
 
 namespace Infra
 {
-    public static class Employees
+    public /*static*/ class Employees
     {
         public static List<Employee> Get(SalesDbContext db)
         {
             return db.Employees.ToList();
+        }
+
+        public Employee Save(Employee e, SalesDbContext db)
+        {
+            db.Employees.Add(e);
+            db.SaveChanges();
+            return e;
         }
         public static List<Employee> GetFakeData()
         {
