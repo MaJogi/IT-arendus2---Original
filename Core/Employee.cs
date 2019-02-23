037a465006179c5f80aa7c57f5f56092396080a2
MaJogi <47116426+MaJogi@users.noreply.github.com>
2019-02-23 11:33:24 +0200

    Added labor13

--- Core/Employee.cs
+++ Core/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,19 +8,23 @@ namespace Core
 {
     public class Employee
     {
-
+        private const string requiredField = "Required field!";
+        private const string lenghtIsToBig = "Length should be less than 20 characters!!";
         public Employee()
         {
         }
 
-        public Employee(string firstName, string lastName = null, int salary = 0)
+        public Employee(string firstName, string lastName, int salary)
         {
-            FirstName = firstName?? string.Empty;
-            LastName = lastName?? string.Empty;
+            FirstName = firstName;
+            LastName = lastName;
             Salary = salary;
         }
         public int EmployeeId { get; set; }
+        [Required(ErrorMessage = requiredField)]
+        [StringLength(20, ErrorMessage = lenghtIsToBig)]
         public string FirstName { get; set; }
+        [StringLength(20, ErrorMessage = lenghtIsToBig)]
         public string LastName { get; set; }
         public int Salary { get; set; }
     }
