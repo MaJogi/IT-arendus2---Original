852d3f2eea24b89371a9bbc0094128db4baba098
MaJogi <47116426+MaJogi@users.noreply.github.com>
2019-03-03 14:34:52 +0200

    String length attribute

--- Week5/EFMVC/ContosoUniversity/Models/Student.cs
+++ Week5/EFMVC/ContosoUniversity/Models/Student.cs
@@ -2,13 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace ContosoUniversity.Models
 {
     public class Student
     {   public int ID { get; set; }
+        [StringLength(50)] //Client side + server side validation, MaxLength provides only server side validation
+        //[RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")] // First will be uppercase and else should be alphabetical
         public string LastName { get; set; }
+        [StringLength(50)]
         public string FirstMidName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
         public ICollection<Enrollment> Enrollments { get; set; }
     }
