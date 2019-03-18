064457fa517a3a8b6a845f976c8d6779802bbf50
MaJogi <47116426+MaJogi@users.noreply.github.com>
2019-03-18 20:53:26 +0200

    Scaffold course model

--- Week7/ContosoUniversity2/Models/Course.cs
+++ Week7/ContosoUniversity2/Models/Course.cs
@@ -18,6 +18,7 @@ namespace ContosoUniversity2.Models
         public int Credits { get; set; }
 
         public int DepartmentID { get; set; }
+        public Department Department { get; set; }
         public ICollection<Enrollment> Enrollments { get; set; }
 
         public ICollection<CourseAssignment> CourseAssignments { get; set; }
