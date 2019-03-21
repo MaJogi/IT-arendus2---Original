65dd572e294e77f42ae7e3b73a376e7939939db6
MaJogi <47116426+MaJogi@users.noreply.github.com>
2019-03-21 23:04:57 +0200

    Before Update the Delete page

--- Week7/ContosoUniversity2/Models/Department.cs
+++ Week7/ContosoUniversity2/Models/Department.cs
@@ -19,10 +19,15 @@ namespace ContosoUniversity2.Models
         public decimal Budget { get; set; }
 
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [Display(Name = "Start Date")]
         public DateTime StartDate { get; set; }
+
         public int? InstructorID { get; set; }
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+
         public Instructor Administrator { get; set; }
         public ICollection<Course> Courses { get; set; }
     }
